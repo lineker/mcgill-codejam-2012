629632d09f4228838c25777136cb009845cf1007
Clarence Leung <cleung0@gmail.com>
2012-11-17 11:34:30 -0500

    Triangular moving average implementation

--- CODE/client/averages/triangular_moving_average.py
+++ CODE/client/averages/triangular_moving_average.py
@@ -10,8 +10,13 @@ class Triangular(Average):
         Formula:
             TMA_t = sum_{i=1}^N SMA_{t-N+1} / N
         """
-        
-        pass
+        self.points.append(point)
+
+        numerator = 0.0
+        for i in xrange(1, len(self.points) + 1):
+            numerator += simple_moving_average(self.time - self.set_size + i)
+
+        return numerator/self.set_size
 
     def add_initial_points(self, point):
         """
@@ -19,5 +24,25 @@ class Triangular(Average):
             for some t <= N:
                 TMA_t = (SMA_t + SMA_{t-1} + ... + SMA_1) / t
         """
-
+        self.points.append(point)
         
+        numerator = 0.0
+        for i in xrange(1, len(self.points) + 1):
+            numerator += simple_moving_average(i)
+        
+        return numerator/len(self.points)
+
+    def simple_moving_average(self, time):
+        """
+        We re-implement SMA in this method for performance improvements over
+        calling the existing SMA class.
+
+        Parameters:
+            time -- The time tick that we want to calculate SMA in
+        """
+
+        if time < self.set_size:
+            return sum(self.points[:time])/time
+        else:
+            # Performance improvement possible, but probably unnecessary
+            return sum(self.points[time - self.set_size : time])/self.set_size
