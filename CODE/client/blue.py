7e3df210987b72c78dfbcc4932cc7f081a9c54b6
ysbecca <ysbecca@gmail.com>
2012-11-17 17:03:22 -0500

    blue turned into a server - test

--- CODE/client/blue.py
+++ CODE/client/blue.py
@@ -3,76 +3,101 @@
 import time
 import socket
 import sys
+import gevent
 from averages import Simple 
 from strategyman import StrategyMan
+from gevent import monkey; monkey.patch_all()
+from time import time
+from bottle import Bottle, request
+from socketio import server, socketio_manage
+from socketio.namespace import BaseNamespace
+from socketio.mixins import BroadcastMixin
 
-stratMan = StrategyMan()
+app = Bottle()
 
-HOST = 'localhost'
-GET = '/rss.xml'
-PORT = 3000
- 
-try:
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error, msg:
-  sys.stderr.write("[ERROR] %s\n" % msg[1])
-  sys.exit(1)
- 
-try:
-  sock.connect((HOST, PORT))
-except socket.error, msg:
-  sys.stderr.write("[ERROR] %s\n" % msg[1])
-  sys.exit(2)
+class Blue(BaseNamespace, BroadcastMixin):
 
-sock.send("H\n")
-count = 0
+    def on_ready(self):
+        def generate_data():
+            while True:
+                data = self.sock.recv(46)
+                string = ""
+                string_del = ""
+                buff = []
+                stor = []
+                index = -1
+                last_string = ""
+                while len(data):
+                    string_del = ""
+                    if last_string != "" and last_string != "C":
+                        data = last_string + data
+                        last_string = ""
+                        
+                    string_del = data.split("|")
+                    index = data.rfind("|")
 
-data = sock.recv(46)
-string = ""
-string_del = ""
-buff = []
-stor = []
-index = -1
-last_string = ""
-while len(data):
-	string_del = ""
-	if last_string != "" and last_string != "C":
-		data = last_string + data
-		last_string = ""
-		
-	string_del = data.split("|")
-	index = data.rfind("|")
+                    if index != -1 & (index + 1) < len(data):
+                        last_string = string_del.pop()
+                    
+                    #print string_del
+                    stor[len(stor):] = string_del
 
-	if index != -1 & (index + 1) < len(data):
-		last_string = string_del.pop()
-	
-	#print string_del
-	stor[len(stor):] = string_del
+                    if len(string_del) > 0:
+                        try:
+                            string_del.remove("")
+                            #detect half-read values
+                            #cut = string_del.
 
-  	if len(string_del) > 0:
-  		try:
-  	  		string_del.remove("")
-  	  		#detect half-read values
-  			#cut = string_del.
+                        except ValueError:
+                            print ""
+                        buff[0:] = map(float, string_del)
+                    print buff
+                    for point in buff:
+                        # pass point to strategyman
+                        self.stratMan.process(point)
+                        # get point and averages and send it
+                        self.emit('data', { "time": time() * 1000, "value": random() })
+                        gevent.sleep(0.5)
 
-  	  	except ValueError:
-  	  		print ""
-	  	buff[0:] = map(float, string_del)
-  	print buff
-  	for point in buff:
-		# pass point to strategyman
-		  stratMan.process(point)
-	  	# update all
+                    data = sock.recv(46)    
 
-	data = sock.recv(46)	
-		
-sock.close()
-	# url = "http://localhost:8080"
- #    data = {'x': data[min], 'y': data[max]}
- #    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
- #    req = urllib2.Request('http://localhost:3000')
- #    req.add_header('Content-Type', 'application/json')
- #    r = requests.post(url, data=json.dumps(data), headers=headers)
+        self.spawn(generate_data)
+        sock.close()
+        print "closing socket"
 
 
-sys.exit(0)
+@app.route('/socket.io/<arg:path>')
+def socketio(*arg, **kw):
+    socketio_manage(request.environ, {'': Data}, request=request)
+    return "out"
+
+if __name__ == '__main__':
+    server.SocketIOServer(
+        ('localhost', 9090), app, policy_server=False).serve_forever()
+    self.sock.send("H\n")
+    """
+    Server to receive the data from the MS exchange, and to output the processed data from the
+    strategy manager to the python server, which sends to the web server.
+
+    Attributes:
+      stratMan -- the reference to the strategy manager
+      HOST -- the host namespace of the MS exchange
+      PORT -- the port name for connection to the MS exchange
+    """
+
+    self.stratMan = StrategyMan()
+    self.HOST = 'localhost'
+    self.PORT = 3000
+
+    try:
+      self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error, msg:
+      sys.stderr.write("[ERROR] %s\n" % msg[1])
+      sys.exit(1)
+     
+    try:
+      self.sock.connect((self.HOST, self.PORT))
+    except socket.error, msg:
+      sys.stderr.write("[ERROR] %s\n" % msg[1])
+      sys.exit(2)
+
