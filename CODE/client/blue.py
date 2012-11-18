04eece351501099d1fd9bf349250cf4df7b26e8a
ysbecca <ysbecca@gmail.com>
2012-11-18 01:15:29 -0500

    blue class now

--- CODE/client/blue.py
+++ CODE/client/blue.py
@@ -15,93 +15,88 @@ import signal
 HOST = 'localhost'
 PORT = 3000
 
-def signal_handler(signal, frame):
-    print 'Ctrl+C detected, killing threads'
-    for i in range(len(threads)):
-        threads[i].exitFlag = True
-    sys.exit(0)
 
-#hack ctrl+D
-signal.signal(signal.SIGINT, signal_handler)
+# #hack ctrl+D
+# signal.signal(signal.SIGINT, signal_handler)
 
-try:
-  exchange_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error, msg:
-  sys.stderr.write("[ERROR] %s\n" % msg[1])
-  sys.exit(1)
- 
-try:
-  exchange_sock.connect((HOST, PORT))
-except socket.error, msg:
-  sys.stderr.write("[ERROR] %s\n" % msg[1])
-  sys.exit(2)
+# def signal_handler(signal, frame):
+#     print 'Ctrl+C detected, killing threads'
+#     for i in range(len(threads)):
+#         threads[i].exitFlag = True
+#     sys.exit(0)
 
-clock = 0
-startFlag = True
+class Blue:
 
-# intialize input and output Q's for each of the 4 strategy managers
-inputQueues = {'sma': Queue.Queue(), 'lwma': Queue.Queue(), 'ema': Queue.Queue(), 'tma': Queue.Queue()}
-outputQueues = {'sma': Queue.Queue(), 'lwma': Queue.Queue(), 'ema': Queue.Queue(), 'tma': Queue.Queue()}
-transactionQueues = {'sma': Queue.Queue(), 'lwma': Queue.Queue(), 'ema': Queue.Queue(), 'tma': Queue.Queue()}
+    def __init__(self, outQueues, startQueue):
+
+        self.flagQueue = startQueue
+        self.clock = 0
 
 
-sma = smaManager(1, 'sma', inputQueues['sma'], clock, outputQueues['sma'], transactionQueues['sma'])
-lwma = lwmaManager(2, 'lwma', inputQueues['lwma'], clock, outputQueues['lwma'], transactionQueues['lwma'])
-ema = emaManager(3, 'ema', inputQueues['ema'], clock, outputQueues['ema'], transactionQueues['ema'])
-tma = tmaManager(4, 'tma', inputQueues['tma'], clock, outputQueues['tma'], transactionQueues['tma'])
+        try:
+            exchange_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error, msg:
+            sys.stderr.write("[ERROR] %s\n" % msg[1])
+            sys.exit(1)
 
-# start manager threads
-threads = []
-threads.append(sma)
-threads.append(lwma)
-threads.append(ema)
-threads.append(tma)
-
-for i in range(len(threads)):
-    threads[i].start()
-    #pass
+        try:
+            exchange_sock.connect((HOST, PORT))
+        except socket.error, msg:
+            sys.stderr.write("[ERROR] %s\n" % msg[1])
+            sys.exit(2)
 
 
-
-flagQueue = Queue.Queue()
-# start sock HEREEEEE, pass the transaction queues and output queues and the flag(?)
-dirtySock = Sock(5, outputQueues['sma'], outputQueues['lwma'], outputQueues['ema'], outputQueues['tma'], flagQueue)
-
-dirtySock.start()
+        # intialize input and output Q's for each of the 4 strategy managers
+        inputQueues = {'sma': Queue.Queue(), 'lwma': Queue.Queue(), 'ema': Queue.Queue(), 'tma': Queue.Queue()}
+        outputQueues = {'sma': Queue.Queue(), 'lwma': Queue.Queue(), 'ema': Queue.Queue(), 'tma': Queue.Queue()}
+        transactionQueues = {'sma': Queue.Queue(), 'lwma': Queue.Queue(), 'ema': Queue.Queue(), 'tma': Queue.Queue()}
 
 
-# poll until startFlag is True
-while not startFlag:
-    # do blah
-    print ""
+        sma = smaManager(1, 'sma', inputQueues['sma'], clock, outputQueues['sma'], transactionQueues['sma'])
+        lwma = lwmaManager(2, 'lwma', inputQueues['lwma'], clock, outputQueues['lwma'], transactionQueues['lwma'])
+        ema = emaManager(3, 'ema', inputQueues['ema'], clock, outputQueues['ema'], transactionQueues['ema'])
+        tma = tmaManager(4, 'tma', inputQueues['tma'], clock, outputQueues['tma'], transactionQueues['tma'])
+
+        # start manager threads
+        threads = []
+        threads.append(sma)
+        threads.append(lwma)
+        threads.append(ema)
+        threads.append(tma)
+
+        for i in range(len(threads)):
+            threads[i].start()
+            #pass
+
+        # poll until startFlag is True
+        while self.flagQueue.isEmpty():
+            # do blah
+            print ""
 
 
-# now we start processing
-print "we got here"
-
-exchange_sock.send("H\n") # start the feed
-byteSize = 1
-data = exchange_sock.recv(byteSize)
-string_del = []
-
-while len(data):
-    #print "data read --> "+data
-    if(data.rfind("C") != -1):
-        print "market closed"
-        break
-    if(data.rfind("|") != -1):
-        point = float(''.join(string_del))
-        print "read point: " + str(point)
-        for q in inputQueues:
-            inputQueues[q].put(point)
-            
+        # now we start processing
+        exchange_sock.send("H\n") # start the feed
+        byteSize = 1
+        data = exchange_sock.recv(byteSize)
         string_del = []
-        clock += 1
-        time.sleep(2)
-    else:
-        string_del.append(data)
 
-    data = exchange_sock.recv(byteSize)
+        while len(data):
+            #print "data read --> "+data
+            if(data.rfind("C") != -1):
+                print "market closed"
+                break
+            if(data.rfind("|") != -1):
+                point = float(''.join(string_del))
+                print "read point: " + str(point)
+                for q in inputQueues:
+                    inputQueues[q].put(point)
+                  
+                string_del = []
+                clock += 1
+            else:
+                string_del.append(data)
 
-    
+            data = exchange_sock.recv(byteSize)
+
+      
 
