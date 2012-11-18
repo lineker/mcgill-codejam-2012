325f3ba8d7724850e5101b8af1bf4bd5b27824df
ysbecca <ysbecca@gmail.com>
2012-11-18 12:37:01 -0500

    json testing

---
+++ CODE/client/testJson.py
@@ -0,0 +1,16 @@
+import sys
+import json, requests
+
+url = 'http://maps.googleapis.com/maps/api/directions/json'
+
+params = dict(
+    origin='Chicago,IL',
+    destination='Los+Angeles,CA',
+    waypoints='Joplin,MO|Oklahoma+City,OK',
+    sensor='false'
+)
+
+resp = requests.get(url=url, params=params)
+data = json.load(resp)
+
+print resp
