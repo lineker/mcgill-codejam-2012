153a71a62af49be1db574c2e99061a32a8ca7bd5
ysbecca <ysbecca@gmail.com>
2012-11-18 03:48:06 -0500

    test to sock

--- CODE/client/sock.py
+++ CODE/client/sock.py
@@ -14,33 +14,35 @@ app = Bottle()
 class Data(BaseNamespace, BroadcastMixin):
 
     def on_ready(self, active):
-        # As client
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("127.0.0.1", 3333))
-        s.send("H\n")
-        data = s.recv(1024)
-        s.close()
+        def start_stuff():
+            # As client
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(("127.0.0.1", 3333))
+            s.send("H\n")
+            data = s.recv(1024)
+            s.close()
 
-        # As server
-        HOST = "localhost"
-        PORT = 9001
-        s.bind((HOST, PORT))
-        s.listen(1)
-        conn, addr = s.accept()
+            # As server
+            HOST = "localhost"
+            PORT = 9001
+            s.bind((HOST, PORT))
+            s.listen(1)
+            conn, addr = s.accept()
 
-        while 1:
-            data = conn.recv(1024)
-            if not data: 
-                break
-            else:
-                parsed_data = data.split("|")
-                if parsed_data[0] == "T":
-                    self.emit("transaction", {"time": parsed_data[1], "type": parsed_data[2], "price": parsed_data[3], "manager": parsed_data[4], "strategy": parsed_data[5]})
-                elif data.split("|")[0] == "A":
-                    self.emit("average", {"price": parsed_data[1], "slow": parsed_data[2], "fast": parsed_data[3]})
+            while 1:
+                data = conn.recv(1024)
+                if not data: 
+                    break
+                else:
+                    parsed_data = data.split("|")
+                    if parsed_data[0] == "T":
+                        self.emit("transaction", {"time": parsed_data[1], "type": parsed_data[2], "price": parsed_data[3], "manager": parsed_data[4], "strategy": parsed_data[5]})
+                    elif data.split("|")[0] == "A":
+                        self.emit("average", {"price": parsed_data[1], "slow": parsed_data[2], "fast": parsed_data[3]})
+            gevent.sleep(0.5)
+            conn.close()
 
-        conn.close()
-        
+        self.spawn(start_stuff)
 
 @app.route('/socket.io/<arg:path>')
 def socketio(*arg, **kw):
